2ea3878baa5e3b2a8620d101780443954a57f7b2
benjicwood <bencharlwood@gmail.com>
2022-02-22 23:05:52 +0000

    100 with grid and colors

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
+<div class="grid">
   <Album v-for="album in albumList" :key="album.position" :album="album" />
+  </div>
 </template>
 
 <script>
@@ -30,5 +32,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+</style>
+
+<style scoped>
+.grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 200px);
+    grid-gap: 10px;
 }
 </style>
--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,12 +1,20 @@
 <template>
     <!-- <img :src="require(`../assets/albumcovers/${album.cover}`)"> -->
-    <div style="border:1px solid black; padding: 10px;">
-        <img :src="getAlbumCover(album.cover)" :alt="album.album" style="width: 200px; height: 200px;">
-        <div>{{album.position}}</div>
-        <div>{{album.artist}}</div>
-        <div>{{album.album}}</div>
-        <div>{{album.year}}</div>
-     </div>
+    <!-- <div style="border:1px solid black; padding: 10px;"> -->
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+            <img :src="getAlbumCover(album.cover)" :alt="album.album" style="width: 200px; height: 200px;">
+            </div>
+            <div class="flip-card-back" :style="{'background-color': album.color ? album.color : 'red'}">
+                <!-- <img :src="getAlbumCover(album.cover)" :alt="album.album" style="width: 200px; height: 200px; filter: brightness(0.5) blur(2px) grayscale(0.5);"> -->
+                <p>{{album.position}}</p>
+                <p>{{album.artist}}</p>
+                <p>{{album.album}}</p>
+                <p>{{album.year}}</p>
+            </div>
+            </div>
+        </div>
 </template>
 
 <script>
@@ -39,3 +47,64 @@ export default {
     },
 }
 </script>
+
+<style scoped>
+.grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 200px);
+    grid-gap: 10px;
+}
+
+.flip-card {
+    width: 200px;
+    height: 200px;
+    display: grid;
+    grid-template-rows: 50% 50%;
+    margin-left: 10px;
+    background-color: transparent;
+    border: 1px solid #f1f1f1;
+    perspective: 1000px; /* Remove this if you don't want the 3D effect */
+}
+
+/* This container is needed to position the front and back side */
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+/* Do an horizontal flip when you move the mouse over the flip box container */
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+/* Position the front and back side */
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
+}
+
+/* Style the front side (fallback if image is missing) */
+.flip-card-front {
+  background-color: #bbb;
+  color: black;
+}
+
+/* Style the back side */
+.flip-card-back {
+  /* background-color: dodgerblue; */
+  color: white;
+  transform: rotateY(180deg);
+  height: 200px;
+}
+/* .flip-card-back::before {
+    background-image: '../assets/albumcovers/turnstileglowon.jpeg';
+    filter: brightness(0.5) blur(2px) grayscale(0.5);
+} */
+</style>
