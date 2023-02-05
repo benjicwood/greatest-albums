82335cb297450d7ab8db5a983d15dff48ce42f61
benjicwood <bencharlwood@gmail.com>
2023-02-05 19:08:07 +0000

    add filter

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
 <template>
+    <input
+      type="text"
+      v-model="filterString"
+      placeholder="Search Artist / Album title..."
+    >
   <p>view: <span @click="changeView('list')" :class="view === 'list' ? 'selected' : ''">list</span> / <span @click="changeView('grid')" :class="view === 'grid' ? 'selected' : ''">grid</span></p>
   <div v-if="view === 'list'">
     <ListItem v-for="album in albumList" :key="album.position" :album="album" />
@@ -21,19 +26,31 @@ export default {
     AlbumCard,
     ListItem,
   },
-  setup() {
-    const albumList = albums;
-    // console.log(albumList)
+  data() {
+    const fullAlbumList = albums;
+    const albumList = fullAlbumList;
+    
     return {
       albumList,
+      fullAlbumList,
+      filterString,
       view,
       changeView,
     }
+  },
+    watch: {
+      filterString: function() {
+          let shortlist = this.fullAlbumList.filter(album => {
+            return album.artist.toLowerCase().includes(filterString.value.toLowerCase()) || album.album.toLowerCase().includes(filterString.value.toLowerCase())
+        })
+        this.albumList = shortlist;
+      }
   }
 }
 
-// let view = 'list';
 let view = ref('grid')
+
+let filterString = ref('')
 
 function changeView(viewChoice) {
   view.value = viewChoice;
@@ -60,5 +77,18 @@ function changeView(viewChoice) {
     display: grid;
     grid-template-columns: repeat(auto-fill, 200px);
     grid-gap: 10px;
+}
+
+input {
+    width: 300px;
+    height: 30px;
+    padding: 10px;
+    border: 1px solid lightgrey;
+    border-radius: 10px;
+    box-shadow: 4px 4px 10px rgba(0,0,0,0.06);
+    margin-bottom: 20px;
+}
+input:focus-visible {
+    outline: none;
 }
 </style>
